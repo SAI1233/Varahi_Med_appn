0365d1b6be63237d014a78b11985675ce816e0ce
Singana Sai Lakshmi <singana.sai.lakshmi@accenture.com>
2025-01-07 00:24:40 +0530

    font fam changes

--- src/app/supervisor_Admin1/barcode-popup/barcode-popup.component.ts
+++ src/app/supervisor_Admin1/barcode-popup/barcode-popup.component.ts
@@ -1,7 +1,9 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Component, AfterViewInit} from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { NgxBarcode6Module } from 'ngx-barcode6';
+import JsBarcode from 'jsbarcode';
+
 
 @Component({
   selector: 'app-barcode-popup',
@@ -10,6 +12,18 @@ import { NgxBarcode6Module } from 'ngx-barcode6';
   templateUrl: './barcode-popup.component.html',
   styleUrls: ['./barcode-popup.component.css']
 })
-export class BarcodePopupComponent {
+export class BarcodePopupComponent implements AfterViewInit{
   emrNumber: string = '1234567890';
+
+  ngAfterViewInit(): void {
+    // Generate Barcode in SVG
+    JsBarcode('#barcode', this.emrNumber, {
+      format: 'CODE128',
+      displayValue: false, // Hide the text below the barcode
+      width: 2,            // Width of bars
+      height: 40,          // Height of the barcode
+      margin: 0            // No margin around the barcode
+    });
+  }
+
 }
